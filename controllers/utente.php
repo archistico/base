bf0f514a91c324ed7edbd52870618e41ba12494a
archistico <info@archistico.com>
2017-12-09 16:17:38 +0100

    utente modifica ok

--- controllers/utente.php
+++ controllers/utente.php
@@ -153,41 +153,100 @@ class UtenteModify {
         $utentefk = 1;
         $errors = [];
 
-        if (isset($_POST['id']) && strlen(trim($_POST['id'])) > 0) {
-            $id = Utilita::PULISCISTRINGA($_POST['id']);
-        } else {
-            $errors[] = 'ID non passato';
-        }
+        // Validazione generale
+        if (isset($_POST['utenteid'], $_POST['denominazione'], $_POST['email'], $_POST['account'], $_POST['password'], $_POST['tipologia']) && strlen(trim($_POST['denominazione'])) > 0) {
 
-        if (isset($_POST['todo']) && strlen(trim($_POST['todo'])) > 0) {
-            $todo = Utilita::PULISCISTRINGA($_POST['todo']);
+            // Validazioni singole
+            if (isset($_POST['utenteid'])) {
+                $id = Utilita::PULISCISTRINGA($_POST['utenteid']);
+            } else {
+                $errors[] = 'id non passato';
+            }
+
+            if (isset($_POST['denominazione']) && strlen(trim($_POST['denominazione'])) > 0) {
+                $denominazione = Utilita::PULISCISTRINGA($_POST['denominazione']);
+            } else {
+                $errors[] = 'Denominazione non passata';
+            }
+
+            if (isset($_POST['email']) && strlen(trim($_POST['email'])) > 0) {
+                $email = Utilita::PULISCISTRINGA($_POST['email']);
+            } else {
+                $errors[] = 'Email non passata';
+            }
+
+            if (isset($_POST['account']) && strlen(trim($_POST['account'])) > 0) {
+                $account = Utilita::PULISCISTRINGA($_POST['account']);
+            } else {
+                $errors[] = 'Account non passato';
+            }
+
+            if (isset($_POST['password']) && strlen(trim($_POST['password'])) > 0) {
+                $password = Utilita::PULISCISTRINGA($_POST['password']);
+            } else {
+                $errors[] = 'Password non passata';
+            }
+
+            if (isset($_POST['tipologia']) && strlen(trim($_POST['tipologia'])) > 0) {
+                $tipologia = Utilita::PULISCISTRINGA($_POST['tipologia']);
+            } else {
+                $errors[] = 'Tipologia non passata';
+            }
+
+            // Dati non obbligatori
+
+            if (isset($_POST['indirizzo'])) {
+                $indirizzo = Utilita::PULISCISTRINGA($_POST['indirizzo']);
+            } else {
+                $indirizzo = '-';
+            }
+
+            if (isset($_POST['cf'])) {
+                $cf = Utilita::PULISCISTRINGA($_POST['cf']);
+            } else {
+                $cf = '-';
+            }
+
+            if (isset($_POST['piva'])) {
+                $piva = Utilita::PULISCISTRINGA($_POST['piva']);
+            } else {
+                $piva = '-';
+            }
+
+            if (isset($_POST['telefono'])) {
+                $telefono = Utilita::PULISCISTRINGA($_POST['telefono']);
+            } else {
+                $telefono = '-';
+            }
+
+
         } else {
-            $errors[] = 'Todo non passato';
+            $errors[] = 'Dati non passati';
         }
 
         if(empty($errors)) {
 
-            if(!TodoEntity::MODIFY($id, $todo)) {
-                $errors[] = 'Errore modifica base dati';
+            if(!UtenteEntity::Modify($id, $denominazione, $indirizzo, $cf, $piva, $telefono, $email, $account, $password, $tipologia)) {
+                $errors[] = 'Errore inserimento nella base dati';
             } else {
-                Flashmessage::ADD($utentefk, 'todo', 'ok', 'Modificato', 'SUCCESS');
+                Flashmessage::ADD($utentefk, 'utente', 'ok', 'Modificato', 'SUCCESS');
             }
         }
 
         if(!empty($errors)) {
             foreach($errors as $testo) {
-                Flashmessage::ADD($utentefk, 'todo', 'Attenzione', $testo, 'ALERT');
+                Flashmessage::ADD($utentefk, 'utente', 'Attenzione', $testo, 'ALERT');
             }
         }
 
         // Rinvia alla pagina
-        header("Location: /todo");
+        header("Location: /utente");
     }
 
     function get($id) {
-        $todo = TodoEntity::ID($id);
-        $linkAnnulla = "/todo";
-        include("views/todomodify.php");
+        $utente = UtenteEntity::ID($id);
+        $linkAnnulla = "/utente";
+        include("views/utentemodify.php");
     }
 }
 
--- views/utentemodify.php
+++ views/utentemodify.php
@@ -31,19 +31,20 @@ Html_default::SHOW_NOTICES(Flashmessage::READ($utentefk, $filename_corrente));
 ?>
     <div class='row'>
         <div class='col-md-12'>
-            <h1>Nuovo utente</h1>
+            <h1>Modifica utente</h1>
             <form action="" method="post">
+                <input type="hidden" name="utenteid" value="<?= $utente['utenteid'] ?>"/>
                 <div class='row'>
                     <div class='col-md-6'>
                         <div class='form-group'>
                             <label for='denominazione'>Denominazione</label>
-                            <input type='text' class='form-control' placeholder='Denominazione' name='denominazione' required>
+                            <input type='text' class='form-control' placeholder='Denominazione' name='denominazione' value="<?= Utilita::DB2HTML($utente['denominazione']) ?>" required>
                         </div>
                     </div>
                     <div class='col-md-6'>
                         <div class='form-group'>
                             <label for='indirizzo'>Indirizzo</label>
-                            <input type='text' class='form-control' placeholder='Indirizzo' name='indirizzo' value='-'>
+                            <input type='text' class='form-control' placeholder='Indirizzo' name='indirizzo' value="<?= Utilita::DB2HTML($utente['indirizzo']) ?>">
                         </div>
                     </div>
                 </div>
@@ -52,13 +53,13 @@ Html_default::SHOW_NOTICES(Flashmessage::READ($utentefk, $filename_corrente));
                     <div class='col-md-6'>
                         <div class='form-group'>
                             <label for='cf'>Codice fiscale</label>
-                            <input type='text' class='form-control' placeholder='Codice fiscale' name='cf' value='-'>
+                            <input type='text' class='form-control' placeholder='Codice fiscale' name='cf' value="<?= Utilita::DB2HTML($utente['cf']) ?>">
                         </div>
                     </div>
                     <div class='col-md-6'>
                         <div class='form-group'>
                             <label for='piva'>Partita iva</label>
-                            <input type='text' class='form-control' placeholder='Partita iva' name='piva' value='-'>
+                            <input type='text' class='form-control' placeholder='Partita iva' name='piva' value="<?= Utilita::DB2HTML($utente['piva']) ?>">
                         </div>
                     </div>
                 </div>
@@ -67,13 +68,13 @@ Html_default::SHOW_NOTICES(Flashmessage::READ($utentefk, $filename_corrente));
                     <div class='col-md-6'>
                         <div class='form-group'>
                             <label for='telefono'>Telefono</label>
-                            <input type='text' class='form-control' placeholder='Telefono' name='telefono' value='-'>
+                            <input type='text' class='form-control' placeholder='Telefono' name='telefono' value="<?= Utilita::DB2HTML($utente['telefono']) ?>">
                         </div>
                     </div>
                     <div class='col-md-6'>
                         <div class='form-group'>
                             <label for='email'>Email</label>
-                            <input type='text' class='form-control' placeholder='Email' name='email' required>
+                            <input type='text' class='form-control' placeholder='Email' name='email' value="<?= Utilita::DB2HTML($utente['email']) ?>" required>
                         </div>
                     </div>
                 </div>
@@ -82,71 +83,36 @@ Html_default::SHOW_NOTICES(Flashmessage::READ($utentefk, $filename_corrente));
                     <div class='col-md-3'>
                         <div class='form-group'>
                             <label for='note'>Nome utente (login)</label>
-                            <input type='text' class='form-control' placeholder='Account' name='account' required>
+                            <input type='text' class='form-control' placeholder='Account' name='account' value="<?= Utilita::DB2HTML($utente['account']) ?>" required>
                         </div>
                     </div>
                     <div class='col-md-3'>
                         <div class='form-group'>
                             <label for='password'>Password</label>
-                            <input type='text' class='form-control' placeholder='Password' name='password' required>
+                            <input type='text' class='form-control' placeholder='Password' name='password' value="<?= Utilita::DB2HTML($utente['password']) ?>" required>
                         </div>
                     </div>
                     <div class='col-md-6'>
                         <div class='form-group'>
                             <label for='tipologia'>Tipologia</label>
                             <select class='form-control' name='tipologia' required>
-                                <option value='Visitatore'>Visitatore</option>
-                                <option value='Normale'>Normale</option>
-                                <option value='Amministratore'>Amministratore</option>
+                                <option value='Visitatore' <?php if($utente['tipologia']=='Visitatore') echo "selected"; ?>>Visitatore</option>
+                                <option value='Normale' <?php if($utente['tipologia']=='Normale') echo "selected"; ?>>Normale</option>
+                                <option value='Amministratore' <?php if($utente['tipologia']=='Amministratore') echo "selected"; ?>>Amministratore</option>
                             </select>
                         </div>
                     </div>
                 </div>
 
                 <div class='row paddingTop20'>
-                    <div class='col-md-12'>
-                        <input type="submit" value="REGISTRA" class='btn btn-block btn-lg btn-success'>
+                    <div class='col-md-6'>
+                        <a class='btn btn-block btn-secondary btn-lg' href='<?= $linkAnnulla ?>'>Annulla</a>
+                    </div>
+                    <div class='col-md-6'>
+                        <input type="submit" value="Modify" class='btn btn-block btn-danger btn-lg'/>
                     </div>
                 </div>
             </form>
-        </div>
-    </div>
-
-    <div class='row paddingTop20'>
-        <div class='col-md-12'>
-            <h1>Lista utenti</h1>
-            <table class='table table-bordered'>
-                <thead>
-                <tr>
-                    <th>Denominazione</th>
-                    <th class='d-none d-md-table-cell'>Email</th>
-                    <th class='d-none d-md-table-cell'>Tipologia</th>
-                    <th class="tdicon"> </th>
-                    <th class="tdicon"> </th>
-                </tr>
-                </thead>
-                <tbody>
-
-                <?php
-                foreach($utenti as $el) {
-                    $id = $el['utenteid'];
-                    $denominazione = Utilita::DB2HTML($el['denominazione']);
-                    $email = Utilita::DB2HTML($el['email']);
-                    $tipologia = Utilita::DB2HTML($el['tipologia']);
-                    ?>
-                    <tr>
-                        <td><?= $denominazione ?></td>
-                        <td><?= $email ?></td>
-                        <td><?= $tipologia ?></td>
-                        <td class="tdicon"><a class="btn btn-warning" href='/utente/modify/<?= $id ?>'><i class='fa fa fa-pencil fa-lg' aria-hidden='true'></i></a></td>
-                        <td class="tdicon"><a class="btn btn-danger" href='/utente/delete/<?= $id ?>'><i class='fa fa fa-trash fa-lg' aria-hidden='true'></i></a></td>
-                    </tr>
-                    <?php
-                }
-                ?>
-
-                </tbody>
-            </table>
         </div>
     </div>
 <?php
